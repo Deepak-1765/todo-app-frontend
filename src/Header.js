5106aeda98b3e2aab6026036aab00a9316e8fd8b
Deepak Chahal <chaliadeepakdaha@gmail.com>
2025-11-21 14:14:48 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 function Header() {
   return (
-    <header className="bg-blue-200 shadow">
+    <header className="bg-red-200 shadow">
       <nav className="flex items-center justify-between p-3">
         <Link to="/" className="text-gray-800 font-bold text-xl">
           <h2 className="text-2xl font-bold text-gray-800 p-2">My To-Do App</h2>
